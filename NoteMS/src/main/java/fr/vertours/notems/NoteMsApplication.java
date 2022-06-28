b66a63c740dafdaf3a9f4f97846ad6d7ea975afb
Ours <vertours.dev@gmail.com>
2022-06-28 13:39:41 +0200

    Creation de NoteMS

---
+++ NoteMS/src/main/java/fr/vertours/notems/NoteMsApplication.java
@@ -0,0 +1,13 @@
+package fr.vertours.notems;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NoteMsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NoteMsApplication.class, args);
+	}
+
+}
---
+++ NoteMS/src/test/java/fr/vertours/notems/NoteMsApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.vertours.notems;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NoteMsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
